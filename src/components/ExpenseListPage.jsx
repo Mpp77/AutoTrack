fa7badde56555410e3ec42fc172626f57e7f8237
Mihoci Paula <paulamihoci1@gmail.com>
2026-04-29 17:48:05 +0300

    update ro/en

--- src/components/ExpenseListPage.jsx
+++ src/components/ExpenseListPage.jsx
@@ -1,20 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import ExpenseList from "./ExpenseList";
 import "../App.css";
-import { useTranslation } from "react-i18next";s
 
 export default function ExpenseListPage() {
   const navigate = useNavigate();
-  const { t } = useTranslation();
 
   return (
     <div className="list-page">
       <button onClick={() => navigate("/home")} className="back-home-btn">
-      ← {t("backHome")}
+        ← Back Home
       </button>
   
       <div className="list-card">
-        <h1 className="list-title">{t("expenseList")}</h1>
+        <h1 className="list-title">Expense List</h1>
         <ExpenseList />
       </div>
     </div>
