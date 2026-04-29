c8364d57c8011d42a6d79cdaa0f454f24b63f718
Mihoci Paula <paulamihoci1@gmail.com>
2026-04-29 17:48:28 +0300

    update ro/en

--- src/components/AddExpensePage.jsx
+++ src/components/AddExpensePage.jsx
@@ -1,20 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import ExpenseForm from "./ExpenseForm";
 import "../App.css";
-import { useTranslation } from "react-i18next";
 
 export default function AddExpensePage() {
   const navigate = useNavigate();
-  const { t } = useTranslation();
 
   return (
     <div className="add-expense-page">
       <button onClick={() => navigate("/home")} className="back-btn">
-      ← {t("backHome")} 
+        ← Back Home
       </button>
   
       <div className="add-expense-card">
-        <h1>{t("addExpense")}</h1>
+        <h1>Add New Expense</h1>
         <ExpenseForm />
       </div>
     </div>
