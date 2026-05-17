85fc4319a7bf24b7202ba1dd8ea58ddf7c3851fc
Mihoci Paula <paulamihoci1@gmail.com>
2026-05-17 14:31:24 +0300

    talon page

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import ExpenseListPage from "./components/ExpenseListPage";
 import CategoryPage from "./components/categoryPage";
 import Settings from "./components/Settings";
 import RemindersPage from "./components/RemindersPage";
-import TalonPage from "./components/talonPage";
+import TalonPage from "./components/TalonPage";
 
 export default function App() {
   return (
